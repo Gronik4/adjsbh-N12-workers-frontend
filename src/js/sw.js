cedc1251e10c395f50ed1b2838e83742eb14c920
Gronik4 <golegni@yandex.ru>
2022-09-08 18:03:55 +0300

    fixed error 1 and error 2 - Does not execute the file sw.js

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,8 +1,7 @@
 
 const apiUrl = 'http://localhost:8080';
 const files = ['./', './app.js', '../css/style.css', '../css/stamp.css', '../img/sqr1.png', '../img/sec2.svg'];
-if ('workbox' in self) { workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []); }
-;
+if ('workbox' in self) { workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []); };
 
 console.log('Started', self);
 self.addEventListener('install', (e) => {
